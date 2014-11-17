76d0fdf17b30219e9edae5c96dd70344081444ee
Russell Wenban <russell@factornine.co.uk>
2014-11-17 21:24:16 +0000

    Use the `snapshot` to get data

--- app/js/contacts/contacts.ctrl.js
+++ app/js/contacts/contacts.ctrl.js
@@ -62,7 +62,12 @@ myAppControllers.controller('ContactsCtrl', ['$scope', 'contactsList', 'fbutil',
 	$scope.testData = function(){
 		
 		// the path for all the contacts
-		var ref = fbutil.ref('contacts');	
+		var ref = fbutil.ref('contacts');
+		
+		console.log('ref: ',ref.path.toString()); // /contacts
+		
+		console.log('key: ', ref.path.toString().key())
+			
 		ref.once('value', function(snapshot) {
 				
 		    var contacts = snapshot.val();
@@ -70,34 +75,26 @@ myAppControllers.controller('ContactsCtrl', ['$scope', 'contactsList', 'fbutil',
 		
 		    snapshot.forEach(function(contact){
                 //console.log('contact | key: ', contact.key()); // will not have key
-				 console.log('21:56 -> child: ', contact);
-              		
-	            console.log('21:56 -> child path: ', contact.path);
-			  
-
-				console.log('contact | value: ', contact.val());
-				
-				var ref = contact.val().contacts.id;
-				
-				console.log('ref: ', ref);
+                console.log('21:56 -> child: ', contact);
+            	console.log('contact | value: ', contact.val());
+				// ths snapshots' val property will have a reference to all the data
+				var id = contact.val().contacts.id;
+				console.log('id: ', id);
 			})
-		
-		  // see if we can modify outside the $scope
-			// THIS IS WHAT I WAS LOOKING FOR - SIMPLE!
-		   var list = contactsList;
 			
-			angular.forEach(list, function(person){
-				// like this we get the $id property!			
-				var ref = fbutil.ref(person.$id);
-				
-				//console.log('path: ',ref.path.toString()); // like `/-JapL5dyvURrthbHDwX5`
-				
-				//var addPath = fbutil.ref('contacts' + ref.path.toString());
-				
-				var addPath = fbutil.ref('contacts/' + person.$id);
+			// see if we can modify outside the $scope
+			// THIS IS WHAT I WAS LOOKING FOR - SIMPLE!
+        var list = contactsList;
+			
+        angular.forEach(list, function(person){
+            // like this we get the $id property!
+            var ref = fbutil.ref(person.$id);
+            //console.log('path: ',ref.path.toString()); // like `/-JapL5dyvURrthbHDwX5`
+            //var addPath = fbutil.ref('contacts' + ref.path.toString());
+            var addPath = fbutil.ref('contacts/' + person.$id);
 							
-				// successfully added data to each node!
-				addPath.child('new').update({'name':'grace'});
+            // successfully added data to each node!
+            addPath.child('new').update({'name':'grace'});
 				
 			});
 		});
